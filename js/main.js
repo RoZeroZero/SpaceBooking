500f4f92fdf7e0ae9cf081aa8e3068256515d3cb
RoZeroZero <r-berko4@mail.ru>
2023-05-14 15:56:00 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	renamed:    css/style.css -> css/style.main.css
    	renamed:    css/swiper.style.css -> css/style.swiper.css
    	deleted:    generate_stage.py
    	modified:   index.html
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -15,11 +15,66 @@ $(document).ready(function() {
         data: { "length": $("input[name='length']").val() },
         success: function(string) {
           $("#the-string").show();
-          document.getElementById("book-park-1").style.backgroundColor = '#F90077';
+          document.getElementById("book-park-1").style.backgroundColor = '#f2efa0';
+          document.getElementById("book-park-2").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-3").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-4").style.backgroundColor = '#f90077';
           $("#the-string input").val(string);
         }
       });
       e.preventDefault();
     });
+
+    $("#book-park-2").click(function(e) {
+      $.ajax({
+        type: "POST",
+        url: "/generator",
+        data: { "length": $("input[name='length']").val() },
+        success: function(string) {
+          $("#the-string").show();
+          document.getElementById("book-park-2").style.backgroundColor = '#f2efa0';
+          document.getElementById("book-park-1").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-3").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-4").style.backgroundColor = '#f90077';
+          $("#the-string input").val(string);
+        }
+      });
+      e.preventDefault();
+    });
+
+    $("#book-park-3").click(function(e) {
+      $.ajax({
+        type: "POST",
+        url: "/generator",
+        data: { "length": $("input[name='length']").val() },
+        success: function(string) {
+          $("#the-string").show();
+          document.getElementById("book-park-3").style.backgroundColor = '#f2efa0';
+          document.getElementById("book-park-2").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-1").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-4").style.backgroundColor = '#f90077';
+          $("#the-string input").val(string);
+        }
+      });
+      e.preventDefault();
+    });
+
+    $("#book-park-4").click(function(e) {
+      $.ajax({
+        type: "POST",
+        url: "/generator",
+        data: { "length": $("input[name='length']").val() },
+        success: function(string) {
+          $("#the-string").show();
+          document.getElementById("book-park-4").style.backgroundColor = '#f2efa0';
+          document.getElementById("book-park-2").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-3").style.backgroundColor = '#f90077';
+          document.getElementById("book-park-1").style.backgroundColor = '#f90077';
+          $("#the-string input").val(string);
+        }
+      });
+      e.preventDefault();
+    });
+
 });
 
