25c7ac61bc4138182d667e0da27661a6078af7e7
RoZeroZero <r-berko4@mail.ru>
2023-05-14 11:49:35 +0700

    Changes to be committed: 	new file:   assets/icon.ico 	new file:   css/style.css 	new file:   css/swiper.style.css 	new file:   db.py 	new file:   index.html 	new file:   js/lib.jquery.js 	new file:   js/lib.swiper.js 	new file:   js/main.js 	new file:   my.db

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+$(document).ready(function() {
+
+    $("#generate-string").click(function(e) {
+      $.post("/generator", {"length": $("input[name='length']").val()})
+       .done(function(string) {
+        $("#the-string").show();
+        $("#the-string input").val(string);
+      });
+      e.preventDefault();
+    });
+
+    $("#replace-string").click(function(e) {
+      $.ajax({
+        type: "PUT",
+        url: "/generator",
+        data: {"another_string": $("#the-string input").val()}
+      })
+      .done(function() {
+        alert("Replaced!");
+      });
+      e.preventDefault();
+    });
+
+    $("#delete-string").click(function(e) {
+      $.ajax({
+        type: "DELETE",
+        url: "/generator"
+      })
+      .done(function() {
+        $("#the-string").hide();
+      });
+      e.preventDefault();
+    });
+
+});
+
